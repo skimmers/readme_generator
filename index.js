5934b56c6adf8f6c717178c2e80a33984b1cb86d
Sharon Kim <skimilk19@gmail.com>
2021-02-19 22:45:48 -0500

    cleaned up comments

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-// TODO: Include packages needed for this application
+// Included packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./generateMarkdown.js');
-
-// TODO: Create an array of questions for user input
-// const questions = [];
 
 //These are my question prompts
 inquirer
@@ -67,11 +64,3 @@ inquirer
         });
     });
 
-// TODO: Create a function to write README file
-//function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-//function init() {}
-
-// Function call to initialize app
-//init();
