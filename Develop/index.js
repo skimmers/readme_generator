48b242f5f2df549179f930c61292a6a37a75a2c7
Sharon Kim <skimilk19@gmail.com>
2021-02-19 21:08:03 -0500

    prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,58 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+
+//These are my question prompts
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of the project?',
+            name: 'Title'
+        },
+        {
+            type: 'input',
+            message: 'What is the description?',
+            name: 'Description'
+        },
+        {
+            type: 'input',
+            message: 'What are the table of contents?',
+            name: 'Table of Contents'
+        },
+        {
+            type: 'input',
+            message: 'What is the project installation instructions?',
+            name: 'Installation'
+        },
+        {
+            type: 'input',
+            message: 'What is the project usage information?',
+            name: 'Usage'
+        },
+        {
+            type: 'list',
+            message: 'What is the project license?',
+            name: 'License',
+            choices: ["MIT", "Mozilla Public License 2.0", "ISC", "Open Software License 3.0", "zLib License", "Academic Free License v3.0"]
+        },
+        {
+            type: 'input',
+            message: 'Who is contributing to the project?',
+            name: 'Contributing'
+        },
+        {
+            type: 'input',
+            message: 'What are your project test instructions?',
+            name: 'Tests'
+        },
+        {
+            type: 'input',
+            message: 'What are your project questions?',
+            name: 'Questions'
+        }
+    ]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
