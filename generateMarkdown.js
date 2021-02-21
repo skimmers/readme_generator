71bc08030ce3387e426edb1d759dbbe560849636
Sharon Kim <skimilk19@gmail.com>
2021-02-20 19:31:41 -0500

    license badge

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,16 +1,17 @@
-//this is what formats and initializes the Readme
-//![${response.license}](https://img.shields.io/apm/l/atomic-design-ui.svg?)\n
+//this is what formats the README
 
 const generateMarkdown = (response) => {
   return `
   
-  ##${response.title}\n
+  ## Title:\n
+  ${response.title}\n
 
   ## Description:\n
   ${response.description}\n
 
   ## Table of Contents:\n
   ${response.contents}\n
+  -[Description](#description)
 
   ## Installation:\n
   ${response.installation}\n
@@ -20,7 +21,7 @@ const generateMarkdown = (response) => {
 
   ## License:\n
   I was not able to display the badge. Any tips would help!
-  ![License](https://img.shields.io/badge/license${response.license}blue.svg "License Badge")\n
+  ![License](https://img.shields.io/badge/License-${response.license}-blue)\n
 
   ## Contributing:\n
   ${response.contribution}\n
